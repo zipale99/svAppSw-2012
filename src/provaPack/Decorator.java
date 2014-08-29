3c79f59fe730dc39453e9764b69f1ec02e5513cc
Alessandro <utente@utente-PC>
2014-08-29 13:53:50 +0200

    Eliminati package di prova

--- src/provaPack/Decorator.java
+++
@@ -1,8 +0,0 @@
-package provaPack;
-
-public class Decorator {
-	//creato nuovo compito
-	//compito 2
-	//compito3
-	//culattone--->lancianouyhjnjh
-}
--- src/provaPack/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package provaPack;
-
-public class HelloWorld {
-	public static void main (String [] args) {
-		System.out.println("Hello World");
-	}	
-}
