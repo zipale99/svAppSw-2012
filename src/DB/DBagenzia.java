2628d17e13197832f7ba6ee6798d19d76a1be528
Sergio <Sergio@Sergio-PC>
2014-09-02 15:25:03 +0200

    dbAggiornato

--- src/DB/DBagenzia.java
+++ src/DB/DBagenzia.java
@@ -1,13 +1,7 @@
 package DB;
 
 import java.sql.*;
-//import java.sql.Connection;
 //import java.util.*;
-//import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
 //import java.util.logging.Level;
 //import java.util.logging.Logger;
 //import java.util.logging.Level;
@@ -20,15 +14,34 @@ public class DBagenzia {
 		Connection conn = DBconnection.getConnection();
 		
 	    try {
+	    	
+	    		///ELIMINA TABELLA UTENTI
+	    	/*String drop = "drop table utenti";
+            PreparedStatement pst = conn.prepareStatement(drop);
+            pst.executeUpdate();//fine cancellazione*/
 	         
-	        	//per creare la tabella
-	        	/*String create = "CREATE TABLE utenti("+
-	        					"username VARCHAR(10),"+
-	        					"password VARCHAR(10),"+
-	        					"tipo VARCHAR(20),"+  
-	        					"PRIMARY KEY (username))";*/
+	        	//CREA TABELLA UTENTI
+	       String create = "CREATE TABLE utenti("+
+	        				"username VARCHAR(10),"+
+	        				"password VARCHAR(10),"+
+	        				"tipo VARCHAR(20),"+  
+	        				"PRIMARY KEY (username))";
 	        	
-	    	/*String create = "CREATE TABLE itinerario("+
+	        PreparedStatement pst = conn.prepareStatement(create);
+            pst.executeUpdate();//fine creazione*/
+            
+            
+            
+            
+            
+            
+            //	ELIMINA TABELLA ITINERARIO
+            /*drop = "drop table ITINERARIO";
+            pst = conn.prepareStatement(drop);
+            pst.executeUpdate();//fine cancellazione*/
+	         
+            //CREA TABELLA ITINERARIO
+	        create = "CREATE TABLE itinerario("+
 					"idItinerario INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
 					"creatorUser VARCHAR(10) NOT NULL,"+
 					"startLoc VARCHAR(20),"+  
@@ -40,22 +53,221 @@ public class DBagenzia {
 					"stato VARCHAR(20),"+
 					"prezzo float(10) not null,"+
 					"PRIMARY KEY (idItinerario),"+
-					"FOREIGN KEY (creatorUser) REFERENCES UTENTI (username))";*/
+					"FOREIGN KEY (creatorUser) REFERENCES UTENTI (username))";
+	        	
+	        pst = conn.prepareStatement(create);
+            pst.executeUpdate();//fine creazione*/
+	        
+            
+
+            
+            //ELIMINA TABELLA STAYTEMPLATE
+        	/*drop = "drop table STAYTEMPLATE";
+            pst = conn.prepareStatement(drop);
+            pst.executeUpdate();//fine cancellazione*/
 	    	
-	    	/*String create = "CREATE TABLE STAYTEMPLATE("+
-	    					"idStayTempl INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
-	    					"idItinerario INTEGER,"+
-	    					"nomeST varchar (20),"+
-	    					"startLoc varchar (20),"+
-	    					"endLoc varchar (20),"+
-	    					"prezzo float(10) not null,"+
-	    					"PRIMARY KEY (idStayTempl),"+
-	    					"FOREIGN KEY (idItinerario) REFERENCES ITINERARIO (idItinerario))";
+            
+            //CREA TABELLA STAYTEMPLATE
+	    	create = "CREATE TABLE STAYTEMPLATE("+
+	    			"idStayTempl INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+	    			"idItinerario INTEGER,"+
+	    			"nomeST varchar (20),"+
+	    			"startLoc varchar (20),"+
+	    			"endLoc varchar (20),"+
+	    			"prezzo float(10) not null,"+
+	    			"PRIMARY KEY (idStayTempl),"+
+	    			"FOREIGN KEY (idItinerario) REFERENCES ITINERARIO (idItinerario))";
 	        	
-	        	PreparedStatement pst = conn.prepareStatement(create);
-	            pst.executeUpdate();//fine creazione*/
+	        	    pst = conn.prepareStatement(create);
+	                pst.executeUpdate();//fine creazione*/
+	                
+	        
+	                
+	                
+	                
+	                
+	                //ELIMINA TABELLA ATTIVITA_ST
+	             /*drop = "drop table ATTIVITA_ST";
+	             pst = conn.prepareStatement(drop);
+	             pst.executeUpdate();//fine cancellazione*/
 	        	
 	        	
+	              //CREA TABELLA ATTIVITA_ST
+	    	   create = "CREATE TABLE ATTIVITA_ST("+
+	    	    		 	"idAttivitaST INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+	    	    		 	"idStayTempl INTEGER NOT NULL,"+
+	    	    		 	"durata integer,"+
+	    	    		 	"idActivity integer,"+
+	    	    		 	"obbligatoria varchar (10),"+
+
+    						"PRIMARY KEY (idAttivitaST),"+
+    						"FOREIGN KEY (idStayTempl) REFERENCES STAYTEMPLATE (idStayTempl))";
+
+        	
+	    	        pst = conn.prepareStatement(create);
+	    	        pst.executeUpdate();//fine creazione*/
+	                
+	    	        
+	    	        
+	    	    
+	    	        
+	    	        //ELIMINA TABELLA leaf
+		             /*drop = "drop table leaf";
+		             pst = conn.prepareStatement(drop);
+		             pst.executeUpdate();//fine cancellazione*/
+		        	
+		        	
+		              //CREA TABELLA leaf
+		    	     create = "CREATE TABLE LEAF("+
+		    	    		 "idLeaf INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+		    	    		    "idStayTemplate integer,"+
+		    	    		    "typeLeaf varchar (20),"+
+		    	    		    "duarata VARCHAR(20),"+
+		    	    		    "veicolo varchar(20),"+
+		    	    		    "startLoc varchar (20),"+
+		    	    		    "startEnd varchar (20),"+
+		    	    		    "description varchar(20),"+
+		    	    		    "category varchar(20),"+
+		    	    		        
+
+		    	    		    "PRIMARY KEY (idLeaf),"+
+		    	    		    "FOREIGN KEY (idStayTemplate) REFERENCES STAYTEMPLATE (idStayTempl))";
+    	
+		    	     pst = conn.prepareStatement(create);
+		    	        pst.executeUpdate();//fine creazione*/
+	    	    
+	    	        
+	                
+		    	        
+		    	        
+	    	
+	    	
+  	      //ELIMINA TABELLA ATTIVITA
+            /*drop = "drop table ATTIVITA";
+            pst = conn.prepareStatement(drop);
+            pst.executeUpdate();//fine cancellazione*/
+       	
+       	
+             //CREA TABELLA attivita
+   	     create = "CREATE TABLE ATTIVITA("+
+   	    		 "idAttivita INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+   	    		 "tipo varchar (20),"+
+   	    		 "citta varchar (20),"+
+   	    		 "descrizione varchar (20),"+
+   	    		 "durata varchar(20),"+
+					"PRIMARY KEY (idAttivita))";
+
+   	        pst = conn.prepareStatement(create);
+   	        pst.executeUpdate();//fine creazione*/
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+		    	 
+		    	        
+		    	        
+		    	      //ELIMINA TABELLA ATTIVITA_LEAF
+			             /*drop = "drop table ATTIVITA_LEAF";
+			             pst = conn.prepareStatement(drop);
+			             pst.executeUpdate();//fine cancellazione*/
+			        	
+			        	
+			              //CREA TABELLA attivita_leaf
+			    	    create = "CREATE TABLE ATTIVITA_LEAF("+
+			    	    		 "idActivityLeaf INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+			    	    		 "idLeaf integer,"+
+			    	    		 "range integer,"+
+			    	    		 "idActivity INTEGER,"+
+
+    							"PRIMARY KEY (idActivityLeaf),"+
+    							"FOREIGN KEY (idLeaf) REFERENCES LEAF (idLeaf),"+
+    							"FOREIGN KEY (idActivity) REFERENCES ATTIVITA (idAttivita))";
+	    	
+			    	        pst = conn.prepareStatement(create);
+			    	        pst.executeUpdate();//fine creazione*/
+		    	
+		    	 
+	                
+	        
+				    	        
+				    	        
+				    	        //ELIMINA TABELLA option
+					             /*drop = "drop table option";
+					             pst = conn.prepareStatement(drop);
+					             pst.executeUpdate();//fine cancellazione*/
+					        	
+					        	
+					              //CREA TABELLA option
+					    	     create = "CREATE TABLE opt ("+
+					    	    		 "idOption INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+					    	    		 "nome varchar(20),"+
+					    	    		 "descrizione varchar (20),"+
+
+   									"PRIMARY KEY (idOption))";
+			    	
+					    	        pst = conn.prepareStatement(create);
+					    	        pst.executeUpdate();//fine creazione*/
+				    	        
+				    	        
+				    	        
+				    	        
+					    
+					    	        
+					    	        //ELIMINA TABELLA option_leaf
+						             /*drop = "drop table option_leaf";
+						             pst = conn.prepareStatement(drop);
+						             pst.executeUpdate();//fine cancellazione*/
+						        	
+						        	
+						              //CREA TABELLA option_leaf
+						    	    create = "CREATE TABLE option_leaf("+
+						    	    		 "idOptLeaf INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+						    	    		 "idLeaf integer,"+
+						    	    		 "idOption integer,"+
+	
+    										"PRIMARY key (idOptLeaf),"+
+    										"FOREIGN KEY (idOption) REFERENCES opt (idOption),"+
+    										"FOREIGN KEY (idLeaf) REFERENCES LEAF (idLeaf))";
+				    	
+						    	        pst = conn.prepareStatement(create);
+						    	        pst.executeUpdate();//fine creazione*/
+					    	        
+					    	        
+						    	        
+						    	  
+						    	        
+						    	        
+						    	        
+						    	        
+						    	      //ELIMINA TABELLA viaggio
+							             /*drop = "drop table viaggio";
+							             pst = conn.prepareStatement(drop);
+							             pst.executeUpdate();//fine cancellazione*/
+							        	
+							        	
+							              //CREA TABELLA viaggio
+							    	    create = "CREATE TABLE viaggio("+
+							    	    		 "idViaggio INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"+
+							    	    		 "creatorUser VARCHAR(10)  NOT NULL,"+	
+							    	    		 "idItinerario INTEGER NOT NULL,"+
+							    	    		 "startDate VARCHAR(20) NOT NULL,"+
+							    	    		 "endDate VARCHAR(20) NOT NULL,"+
+							    	    		 "costo VARCHAR(20) NOT NULL,"+
+							    	    		 "nPersone VARCHAR(20) NOT NULL,"+
+							    	    		 "acquistato varchar(5) not null,"+
+
+    											  "PRIMARY KEY (idViaggio),"+
+    											  "FOREIGN KEY (creatorUser) REFERENCES UTENTI (username),"+
+    											  "FOREIGN KEY (idItinerario) REFERENCES ITINERARIO (idItinerario))";
+					    	
+							    	      pst = conn.prepareStatement(create);
+							    	      pst.executeUpdate();//fine creazione*/
+						    	        
+						    	
+					   
+				    	        
 	        	//per cancellare tabella
 	        	/*String drop = "drop table itinerario";
 	            PreparedStatement pst = conn.prepareStatement(drop);
@@ -122,7 +334,7 @@ public class DBagenzia {
 	            System.out.println(gio);//fine della stampa di una singola tupla*/
 	                  	
 	        	//per stampare tutte le tuple
-	    Statement st = conn.createStatement();
+	    /*Statement st = conn.createStatement();
 	    String select = "SELECT * FROM staytemplate";
 	    ResultSet rs = st.executeQuery(select);
 	            //stampa una singola tupla senza il while sotto
@@ -140,6 +352,7 @@ public class DBagenzia {
 	        	
 	    //rs.close();
 	    //st.close();
+        //pst.close();
 	    conn.close();    
 	            
 	  }
